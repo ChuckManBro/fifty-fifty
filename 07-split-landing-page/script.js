bddebc435c5535212c2d471084ebebc352c5cb31
Charles Cook <Chuck@cook-macbook.local>
2023-05-21 09:53:38 -0600

    Exercise 7 added

---
+++ 07-split-landing-page/script.js
@@ -0,0 +1,14 @@
+'use strict';
+// 50 PROJECTS IN 50 DAYS
+
+const left = document.querySelector(`.left`);
+const right = document.querySelector(`.right`);
+const container = document.querySelector(`.container`);
+
+left.addEventListener('mouseenter', () => container.classList.add(`hover-left`));
+
+left.addEventListener('mouseleave', () => container.classList.remove(`hover-left`));
+
+right.addEventListener('mouseenter', () => container.classList.add(`hover-right`));
+
+right.addEventListener('mouseleave', () => container.classList.remove(`hover-right`));
