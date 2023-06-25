0cc46b1e5b02fbee679aad3637d39f6045fcb409
ChuckManBro <118929288+ChuckManBro@users.noreply.github.com>
2023-06-25 16:37:19 -0600

    Exercise 25 added

---
+++ 25-sticky-navbar/script.js
@@ -0,0 +1,13 @@
+'use strict';
+// 50 PROJECTS IN 50 DAYS
+
+const nav = document.querySelector(`.nav`);
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+	if (window.scrollY > nav.offsetHeight + 150) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
+}
