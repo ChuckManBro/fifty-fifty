6f91e7bc071b7abb2cffd9a8ca88a9294999536b
Charles Cook <Chuck@cook-macbook.local>
2023-07-02 12:40:28 -0600

    Exercise 37 added

---
+++ 37-pokedex/script.js
@@ -0,0 +1,71 @@
+'use strict';
+// 50 PROJECTS IN 50 DAYS
+
+const poke_container = document.querySelector(`#poke-container`);
+const pokemon_count = 150;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#defde0',
+	electric: '#fcf7de',
+	water: '#def3fd',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#f5f5f5',
+	fighting: '#e6e0d4',
+	normal: '#f5f5f5',
+};
+
+const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {
+	for (let i = 1; i <= pokemon_count; i++) {
+		await getPokemon(i);
+	}
+};
+
+const getPokemon = async (id) => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const data = await res.json();
+	createPokemonCard(data);
+};
+
+const createPokemonCard = (pokemon) => {
+	const pokemonEl = document.createElement('div');
+	pokemonEl.classList.add('pokemon');
+
+	// let thisId = pokemon.id.toString()
+	// if(thisId.length == 1) thisId = `00${thisId}`
+	// if(thisId.length == 2) thisId = `0${thisId}`
+	const thisId = pokemon.id.toString().padStart(3, '0');
+
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+	const poke_types = pokemon.types.map((type) => type.type.name);
+	const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+	const color = colors[type];
+
+	pokemonEl.style.backgroundColor = color;
+
+	const pokemonInnerHTML = `
+  <div class="img-container">
+    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${thisId}.png" alt="" />
+  </div>
+  <div class="info">
+    <span class="number">#${thisId}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${type}</span></small>
+  </div>
+  `;
+
+	pokemonEl.innerHTML = pokemonInnerHTML;
+
+	poke_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();
